91934b69ac1e1b42b1aa6a4d9e951a2be1d63920
zhukaikang <zkk_nomore@126.com>
2019-10-27 18:02:28 -0400

    bigtable

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 
+	"cloud.google.com/go/bigtable"
 	"cloud.google.com/go/storage"
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/pborman/uuid"
@@ -32,11 +33,12 @@ type Post struct {
 }
 
 const (
-	INDEX    = "around"
-	TYPE     = "post"
-	DISTANCE = "200km"
-	ES_URL   = "http://35.232.235.186:9200/"
-
+	INDEX       = "around"
+	TYPE        = "post"
+	DISTANCE    = "200km"
+	ES_URL      = "http://35.232.235.186:9200/"
+	PROJECT_ID  = "glossy-aloe-253018"
+	BT_INSTANCE = "around-post"
 	BUCKET_NAME = "post-images-253018"
 )
 
@@ -146,6 +148,9 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	p.Url = attrs.MediaLink
 	//save to ES
 	saveToES(p, id)
+
+	// save to BigTable
+	saveToBigTable(p, id)
 }
 
 func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
@@ -180,6 +185,30 @@ func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*stor
 	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
 
 	return obj, attrs, err
+}
+
+func saveToBigTable(p *Post, id string) {
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation() // one row
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 func saveToES(p *Post, id string) {
